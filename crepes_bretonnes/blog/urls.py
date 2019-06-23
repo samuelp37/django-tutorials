e6252835759aa5840a645d222c5b54df7d2f7913
spierre37 <samuel.pierre37@yahoo.fr>
2019-06-23 21:39:44 +0200

    * Correcting the part concerning pictures' management

--- crepes_bretonnes/blog/urls.py
+++ crepes_bretonnes/blog/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path('', views.accueil, name='accueil'),
@@ -11,5 +9,3 @@ urlpatterns = [
     path('insert_contact/', views.nouveau_contact, name='insert_contact'),
     path('voir_contacts/', views.voir_contacts, name='voir_contact'),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
